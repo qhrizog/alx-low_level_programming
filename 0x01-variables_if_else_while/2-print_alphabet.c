fe82b32cab8a99587d1402abf86f610ef398177a
Nwaekpe Ogbonna <qhrizog@gmail.com>
2022-07-01 10:32:19 -0700

    Insomnia Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,14 +10,12 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-  char letter;
-  
-  for (letter = 'a'; letter <= 'z'; letter++)
-    putchar(letter);
-
-  putchar('\n');
-
-  return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
