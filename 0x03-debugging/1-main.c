38345421fb1001adc564cc75fc2d1802aaeaef90
Nwaekpe Ogbonna <qhrizog@gmail.com>
2022-07-06 21:49:57 -0700

    Loop Avoided

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -16,23 +16,15 @@ printf("Infinite loop incoming :(\n");
 
 
 
-//i = 0;
+i = 0;
+/**
+while (i < 10)
+{
+putchar(i);
+}
+*/
 
-
-  /**  
-  while (i < 10)
-    
-    {
-      
-      putchar(i);
-      
-    }
-  */
-
-  
 printf("Infinite loop avoided! \\o/\n");
-
-
 
 return (0);
 
