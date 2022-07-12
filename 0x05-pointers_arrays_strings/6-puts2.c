8c9f9f3c410a62aeb27dc029d50b09a7c9e77411
Nwaekpe Ogbonna <qhrizog@gmail.com>
2022-07-12 01:24:40 -0700

    Puts Again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * puts2 - prints one char out of 2
- * @str: type
- *
+ * puts2 - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
  */
+
 void puts2(char *str)
 {
-int i, j = 0;
+int i = 0;
 while (str[i] != '\0')
 {
-i++;
-}
-while (j < i)
+if (i % 2 == 0)
 {
-_putchar(str[j]);
-j = j + 2;
+_putchar(str[i]);
+}
+i++;
 }
 _putchar('\n');
 }
