31279405c05d6043637e3a08d990d79b9a90fabc
Nwaekpe Ogbonna <qhrizog@gmail.com>
2022-07-15 09:03:58 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-  int a = -1, i;
-  for (i = 0; dest[i] != '\0'; i++)
-    ;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++);
 
-  do {
-    a++;
-    dest[i] = src[a];
-    i++;
-  } while (src[a] != '\0');
-
-  return (dest);
+do {
+a++;
+dest[i] = src[a];
+i++;
+} while (src[a] != '\0');
+return (dest);
 }
