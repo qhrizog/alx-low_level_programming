1f03c01f698f0618370c53fbf4e54b4911f1f6fd
Nwaekpe Ogbonna <qhrizog@gmail.com>
2022-07-15 10:09:00 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
-#ifndef ALX_FILE
-#define ALX_FILE
+#ifndef FILE_ALX
+#define FILE_ALX
 
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
+void print_number(int n);
+int _putchar(char c);
+void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -11,9 +13,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-int main(void);
 
 #endif
