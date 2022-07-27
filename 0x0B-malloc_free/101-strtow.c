0c8d691fe740030978224fc47e9acdfe2137cb27
Nwaekpe Ogbonna <qhrizog@gmail.com>
2022-07-26 22:01:36 -0700

    fxs

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,77 +1,139 @@
+#include "main.h"
 #include <stdlib.h>
+
 /**
- * strtow - char
- * @str: pointer to string params
- * Return: char
+ * wrdcnt - counts the number of words in a string
+ * @s: string to count
+ *
+ * Return: int of number of words
  */
+
+int wrdcnt(char *s)
+
+{
+
+int i, n = 0;
+
+
+
+for (i = 0; s[i]; i++)
+
+{
+
+if (s[i] == ' ')
+
+{
+
+if (s[i + 1] != ' ' && s[i + 1] != '\0')
+
+n++;
+
+}
+
+else if (i == 0)
+
+n++;
+
+}
+
+n++;
+
+return (n);
+
+}
+
+
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings
+ */
+
 char **strtow(char *str)
-{
-int i = 0, j = 0, k = 0;
-int len = 0, count = 0;
-char **f, *col;
 
-if (!str || !*str)
 {
-return (NULL);
-}
 
-while (*(str + i))
-{
-if (*(str + i) != ' ')
-{
-if (*(str + i + 1) == ' ' || *(str + i + 1) == 0)
-{
-count += 1;
-}
-}
-i++;
-}
-if (count == 0)
-{
+int i, j, k, l, n = 0, wc = 0;
+
+char **w;
+
+
+
+if (str == NULL || *str == '\0')
+
 return (NULL);
-}
-count += 1;
-f = malloc(sizeof(char *) * count);
-if (!f)
-{
+
+n = wrdcnt(str);
+
+if (n == 1)
+
 return (NULL);
-}
+
+w = (char **)malloc(n * sizeof(char *));
+
+if (w == NULL)
+
+return (NULL);
+
+w[n - 1] = NULL;
+
 i = 0;
-while (*str)
-{
-while (*str == ' ' && *str)
-{
-str++;
-}
-len = 0;
-while (*(str + len) != ' ' && *(str + len))
-{
-len += 1;
-}
-len += 1;
-col = malloc(sizeof(char) * len);
-if (!col)
-{
-for (k = j - 1; k >= 0; k--)
-{
-free(f[k]);
-}
-free(f);
-return (NULL);
-}
 
-for (k = 0; k < (len - 1);  k++)
-{
-*(col + k) = *(str++);
-}
-*(col + k) = '\0';
-*(f + j) = col;
+while (str[i])
 
-if (j < (count - 1))
 {
+
+if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+
+{
+
+for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+
+;
+
 j++;
+
+w[wc] = (char *)malloc(j * sizeof(char));
+
+j--;
+
+if (w[wc] == NULL)
+
+{
+
+for (k = 0; k < wc; k++)
+
+free(w[k]);
+
+free(w[n - 1]);
+
+free(w);
+
+return (NULL);
+
 }
+
+for (l = 0; l < j; l++)
+
+w[wc][l] = str[i + l];
+
+w[wc][l] = '\0';
+
+wc++;
+
+i += j;
+
 }
-*(f + j) = NULL;
-return (f);
+
+else
+
+i++;
+
 }
+
+return (w);
+
+}
+
