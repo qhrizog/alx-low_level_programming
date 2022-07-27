90dae2cbbd016d6c0659dbf49717a8efedfaa064
Nwaekpe Ogbonna <qhrizog@gmail.com>
2022-07-26 22:11:24 -0700

    Fixed

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -50,7 +50,7 @@ if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
 j++;
-w[wc] = (char *)malloc(j * sizeof(char));
+w[wc] = (char *) malloc(j * sizeof(char));
 j--;
 if (w[wc] == NULL)
 {
